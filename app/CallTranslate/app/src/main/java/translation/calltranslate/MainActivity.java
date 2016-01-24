66a5444836337c17c7c5e0f27070499d0ddd79c7
Mohamed <mmmoussa@uwaterloo.ca>
2016-01-24 07:21:25 -0500

    Added menu item to link to contacts page

--- app/CallTranslate/app/src/main/java/translation/calltranslate/MainActivity.java
+++ app/CallTranslate/app/src/main/java/translation/calltranslate/MainActivity.java
@@ -1,35 +1,16 @@
 package translation.calltranslate;
 
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.SearchView;
-import android.widget.Toast;
 
 import com.firebase.client.Firebase;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -82,5 +63,28 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_contacts:
+                Intent contactsIntent = new Intent(context, ContactActivity.class);
+                startActivity(contactsIntent);
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
